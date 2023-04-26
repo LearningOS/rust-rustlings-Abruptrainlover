448ce7451192942b74d2a042a55cd5964f299e97
zhiwie yang <87537258+Abruptrainlover@users.noreply.github.com>
2023-04-26 18:00:46 +0800

    exercises completed
    Completed All rustlings exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,12 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(prz:i32)->i32 {
+    if(prz>40){ prz}
+    else {2*prz}
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
